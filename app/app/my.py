cab59a24a5ce054ced3b0a19d6ab322780de8634
sameersultan41 <sameer@linkedunion.com>
2024-12-29 13:41:24 +0500

    psycopg2 config

---
+++ app/app/my.py
@@ -0,0 +1,6 @@
+# Description: This is a simple calculator module
+def add_two_numbers(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
---
+++ app/app/tests.py
@@ -0,0 +1,13 @@
+from django.test import SimpleTestCase
+from app import my
+
+
+# Create your tests here.
+class CalTest(SimpleTestCase):
+    def test_add(self):
+        res = my.add_two_numbers(3, 4)
+        self.assertEqual(res, 7)
+        
+    def subtract(self):
+        res = my.subtract(10, 5)
+        self.assertEqual(res, 5)
