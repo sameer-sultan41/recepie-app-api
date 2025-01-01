75e170e4790db7a1df8aae6a34d42c4a367a77ff
sameersultan41 <sameer@linkedunion.com>
2025-01-01 23:00:30 +0500

    create custom user model

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,34 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """Test creating a new user with an email is successful"""
+        email = "dummy@example.com"
+        password = "testpass123"
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized"""
+        sample_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['test2@EXAMPLE.com', 'test2@example.com'],
+            ['Test3@Example.com', 'Test3@example.com'],
+            ['TEST4@EXAMPLE.COM', 'TEST4@example.com'],
+            ['test5@example.COM', 'test5@example.com']
+        ]
+
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'test123')
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
