e164c546a589b5ccfe37c385b37c7fc81b02e8eb
sameersultan41 <sameer@linkedunion.com>
2025-01-12 11:32:52 +0500

    create me user endpoint

--- app/user/urls.py
+++ app/user/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from user.views import CreateUserView
+from user.views import CreateUserView, CreateTokenView, ManageUserView
 
 app_name = 'user'  # This defines the namespace
 
 urlpatterns = [
     path('create/', CreateUserView.as_view(), name='create'),
+    path('token/', CreateTokenView.as_view(), name='token'),
+    path('me/', ManageUserView.as_view(), name='me')
 ]
--- app/user/views.py
+++ app/user/views.py
@@ -1,6 +1,25 @@
-from rest_framework import generics
-from user.serializers import UserSerializer
+from rest_framework import generics, authenticatioin, permissions
+from rest_framework.authtoken.views import ObtainAuthToken
+from user.serializers import (
+    UserSerializer,
+    AuthTokenSerializer
+    )
+from rest_framework.settings import api_settings
 
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    authenication_classes = [authenticatioin.TokenAuthentication]
+    permissions_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
