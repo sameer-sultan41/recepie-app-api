c0a8a28e21aa882da11adf5dbdb071646f40a49c
sameersultan41 <sameer@linkedunion.com>
2025-01-17 22:05:50 +0500

    recepie  tesr case fix

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,23 +2,23 @@ from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from core.models import Recipe
-from recipe import serializer
+from recipe import serializers
 # Create your views here.
 
 
 class RecipeViewSet(viewsets.ModelViewSet):
     """Manage recipes in the database"""
-    serializer_class = serializer.RecipeDetailSerializer
+    serializer_class = serializers.RecipeDetailSerializer
     queryset = Recipe.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
         """Retrieve the recipes for the authenticated user"""
-        return self.queryset.filter(user=self.request.user).order_by(-'id')
+        return self.queryset.filter(user=self.request.user).order_by('-id')
 
     def get_serializer_class(self):
         """Return appropriate serializer class"""
         if self.action == 'list':
-            return serializer.RecipeSerializer
+            return serializers.RecipeSerializer
         return self.serializer_class
