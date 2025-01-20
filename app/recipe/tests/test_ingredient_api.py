b7bc30d2f6de4b1fdbc0dcbbb620a250a1d3d18e
sameersultan41 <sameer@linkedunion.com>
2025-01-20 21:56:13 +0500

    add end point and testcase to add ingredients

---
+++ app/recipe/tests/test_ingredient_api.py
@@ -0,0 +1,56 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+from core.models import  Ingredient
+from recipe.serializers import IngredientSerializer
+
+
+INGREDIENT_URL = reverse('recipe:ingredient-list')
+
+
+def create_user(email='user@example.com', password='testpass123'):
+    return get_user_model().objects.create_user(email=email, password=password)
+
+
+class PublicIngredientAPITests(TestCase):
+    """Test unauthenticated ingredient API access"""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_auth_required(self):
+        """Test that authentication is required"""
+        res = self.client.get(INGREDIENT_URL)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateIngredientAPITests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = create_user()
+        self.client.force_authenticate(self.user)
+
+    def test_retrieve_ingredient_list(self):
+        """Test retrieving a list of ingredients"""
+        Ingredient.objects.create(user=self.user, name='Kale')
+        Ingredient.objects.create(user=self.user, name='Salt')
+
+        res = self.client.get(INGREDIENT_URL)
+
+        ingredients = Ingredient.objects.all().order_by('-name')
+        serializer = IngredientSerializer(ingredients, many=True)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_ingredients_limited_to_user(self):
+        """Test that only ingredients for authenticated user are returned"""
+        user2 = create_user(email='user2@example.com', password='testpass123')
+        Ingredient.objects.create(user=user2, name='Vinegar')
+        ingredient = Ingredient.objects.create(user=self.user, name='Tumeric')
+        res = self.client.get(INGREDIENT_URL)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 1)
+        self.assertEqual(res.data[0]['name'], ingredient.name)
+        self.assertEqual(res.data[0]['id'], ingredient.id)
--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -7,6 +7,7 @@ app_name = 'recipe'  # This defines the namespace
 router = DefaultRouter()
 router.register('recipe', views.RecipeViewSet)
 router.register('tags', views.TagViewSet)
+router.register('ingredients', views.IngredientViewSet)
 
 urlpatterns = [
     path('create/', include(router.urls)),
